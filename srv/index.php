49c965c51f22b09555b4476dba525d9b53d44cc4
Goldman40 <goldman40@laposte.net>
2014-08-26 17:28:04 +0200

    Form
    Add form

--- srv/index.php
+++ srv/index.php
@@ -1,9 +1,17 @@
 <?php 
- include('getUuid.php'); 
-
-	$p = $_GET['p'];
-
-	getUuid($p);
-	
- echo $uuid ;
+ include('getUuid.php');
+ 
+if ( $_GET['p'] == NULL ) { 
+?>
+<html>
+<form action="index.php" method="GET">
+	<input type="text" name="p"/>
+	<input type="submit" value="Send"/>
+</form>
+ <?php
+} else {
+ echo '<br><br>Your Minecraft uuid is :';
+	getUuid($_GET['p']);
+ echo '<br><br><a href="index.php">Send another uuid</a>';
+ }
  ?>
