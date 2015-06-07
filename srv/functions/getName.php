7b2213f79496bd863d2bbe83f406e6f3ab3116ae
Goldman40 <goldman40@laposte.net>
2015-06-07 12:58:17 +0200

    Update getName.php

--- srv/functions/getName.php
+++ srv/functions/getName.php
@@ -4,8 +4,6 @@ function getName ($uuid)
 
 	$json = file_get_contents('https://sessionserver.mojang.com/session/minecraft/profile/'.$uuid.'');
 
-		(json_decode($json));
-
 	$parsed_json = json_decode($json);
 	$name = $parsed_json->{'name'};
 
