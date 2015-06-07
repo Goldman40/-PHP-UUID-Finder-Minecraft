29b59044b1964e59707c0497c5eb8f0cde424cb8
Goldman40 <goldman40@laposte.net>
2015-06-07 12:58:33 +0200

    Update getUuid.php

--- srv/functions/getUuid.php
+++ srv/functions/getUuid.php
@@ -4,8 +4,6 @@ function getUuid ($pseudo)
 
 	$json = file_get_contents('https://api.mojang.com/users/profiles/minecraft/'.$pseudo.'');
 
-		(json_decode($json));
-
 	$parsed_json = json_decode($json);
 	$uuid = $parsed_json->{'id'};
 
